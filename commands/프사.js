4ba32b68ea650f7b331c3a7abf3b84e54a162a00
TreeKR <johanbag290@gmail.com>
2020-11-17 11:30:06 +0900

    profile photo mention & embed

--- commands/프사.js
+++ commands/프사.js
@@ -10,5 +10,16 @@ const fu = require('../util/file_util')
  * @param {discord.Message} msg
  */
 module.exports.onCommand = (client, msg) => {
-  msg.channel.send(msg.author.displayAvatarURL())
+  if(msg.mentions.members.first() !== undefined) {
+    let embed = new discord.MessageEmbed()
+    embed.setTitle(msg.mentions.members.first().user.tag + '님의 프로필사진')
+    embed.setImage(msg.mentions.members.first().user.displayAvatarURL())
+    msg.channel.send(embed)
+    return
+  }
+  let embed = new discord.MessageEmbed()
+  embed.setTitle(msg.author.tag + '님의 프로필사진')
+  embed.setImage(msg.author.displayAvatarURL())
+  msg.channel.send(embed)
+  return
 }
